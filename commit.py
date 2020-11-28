d76fef73790529595def7fd136ecf8b83390e84d
arnav-verma22 <arn.verma22@gmail.com>
2020-11-28 15:22:45 +0530

    this is an automated commit

--- commit.py
+++ commit.py
@@ -5,3 +5,7 @@ f.write("#auto generated file \n")
 f.write("#auto generated file testing ")
 f.close()
 #"w" operation truncates previous file
+
+os.system('cmd /c "git add commit.py"')
+os.system('cmd /c "git commit -m "this is an automated commit""')
+os.system('cmd /c "git push"')
