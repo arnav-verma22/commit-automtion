5c2ffd546649ea9f2a7c12fbd386edca78946fb7
arnav-verma22 <arn.verma22@gmail.com>
2020-11-28 15:01:05 +0530

    deleted testing

--- commit.py
+++ commit.py
@@ -1,14 +1,5 @@
 import os
 
-file = open("file.py", "w+")
-print(file)
-file.write("#auto generated file testing")
-file.close()
-
-f = open("file.py", "w")
-f.write("#auto generated file \n")
-f.write("#auto generated file testing ")
-f.close()
 f = open("file.py", "a+")
 f.write("#auto generated file \n")
 f.write("#auto generated file testing ")
