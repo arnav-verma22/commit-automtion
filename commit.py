70938f07e79a3d4f412dd916ff00e18c825a28c9
arnav-verma22 <arn.verma22@gmail.com>
2020-11-28 16:01:24 +0530

    process complete

--- commit.py
+++ commit.py
@@ -1,11 +1,12 @@
 import os
 
-f = open("file.py", "a+")
-f.write("#auto generated file \n")
-f.write("#auto generated file testing ")
-f.close()
-#"w" operation truncates previous file
+for i in range(6):
+    f = open("file.py", "a+")
+    f.write("#auto generated file \n")
+    f.write("#auto generated script for testing\n")
+    f.close()
+    #"w" operation truncates previous file
 
-os.system('cmd /c "git add commit.py"')
-os.system('cmd /c "git commit -m "this is an automated commit""')
-os.system('cmd /c "git push"')
+    os.system('cmd /c "git add file.py"')
+    os.system('cmd /c "git commit -m "this is an automated commit""')
+    os.system('cmd /c "git push"')
